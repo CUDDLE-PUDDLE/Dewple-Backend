fc5093e9d7faaec6c87ea1787197fc1b70c72eec
hyunn522 <tjgus52213@gmail.com>
2026-02-12 01:17:26 +0900

    chore: AppApiAuthApplication에 jpa 및 entity 관련 멀티 모듈 설정 추가

--- apps/app-api-auth/src/main/java/com/dewple/app_api_auth/AppApiAuthApplication.java
+++ apps/app-api-auth/src/main/java/com/dewple/app_api_auth/AppApiAuthApplication.java
@@ -2,8 +2,12 @@ package com.dewple.app_api_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.dewple")
+@EnableJpaRepositories(basePackages = "com.dewple")
+@EntityScan(basePackages = "com.dewple")
 public class AppApiAuthApplication {
 
 	public static void main(String[] args) {
