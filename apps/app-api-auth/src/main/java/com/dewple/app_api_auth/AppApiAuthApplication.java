ae669f4a9d2dbccd3da8c4837d176b80cb11fab8
hyunn522 <tjgus52213@gmail.com>
2026-02-14 16:05:16 +0900

    fix: 웹 레이어 테스트를 위해 JpaConfig 추가

--- apps/app-api-auth/src/main/java/com/dewple/app_api_auth/AppApiAuthApplication.java
+++ apps/app-api-auth/src/main/java/com/dewple/app_api_auth/AppApiAuthApplication.java
@@ -2,12 +2,8 @@ package com.dewple.app_api_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.dewple")
-@EnableJpaRepositories(basePackages = "com.dewple")
-@EntityScan(basePackages = "com.dewple")
 public class AppApiAuthApplication {
 
 	public static void main(String[] args) {
