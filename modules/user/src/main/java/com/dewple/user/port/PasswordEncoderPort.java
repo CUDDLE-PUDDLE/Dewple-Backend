1b739c469b24ae89cf1c0e9a521877936bab62be
hyunn522 <tjgus52213@gmail.com>
2026-02-18 21:23:02 +0900

    feat: 비밀번호 인코딩 기능 구현

---
+++ modules/user/src/main/java/com/dewple/user/port/PasswordEncoderPort.java
@@ -0,0 +1,8 @@
+package com.dewple.user.port;
+
+public interface PasswordEncoderPort {
+
+    String encode(String rawPassword);
+
+    boolean matches(String rawPassword, String encodedPassword);
+}
