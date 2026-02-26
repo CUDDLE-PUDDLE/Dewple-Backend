7e9ed3b6532eefd3b264a8128a4a8d54c3097134
yuna <yjeong1217@gmail.com>
2026-02-27 03:19:43 +0900

    feat: 이전 공고 지원 양식 불러오기 API 및 테스트 추가

--- modules/recruitment/src/main/java/com/dewple/recruitment/repository/RecruitmentPostingRepository.java
+++ modules/recruitment/src/main/java/com/dewple/recruitment/repository/RecruitmentPostingRepository.java
@@ -1,11 +1,16 @@
 package com.dewple.recruitment.repository;
 
+import com.dewple.common.enums.RecruitmentStatus;
 import com.dewple.recruitment.entity.RecruitmentPosting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RecruitmentPostingRepository extends JpaRepository<RecruitmentPosting, Long> {
 
+    Optional<RecruitmentPosting> findFirstByClubIdAndRecruitmentStatusNotOrderByCreatedAtDesc(
+            Long clubId, RecruitmentStatus status);
 }
 
