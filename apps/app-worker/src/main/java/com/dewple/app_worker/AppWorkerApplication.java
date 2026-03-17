0027425bb45dacc4875251d0e901c3aa53067662
hyunn522 <tjgus52213@gmail.com>
2026-03-17 14:01:09 +0900

    fix: worker 서버의 베이스 패키지 설정 변경

--- apps/app-worker/src/main/java/com/dewple/app_worker/AppWorkerApplication.java
+++ apps/app-worker/src/main/java/com/dewple/app_worker/AppWorkerApplication.java
@@ -3,7 +3,10 @@ package com.dewple.app_worker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.dewple")
+@SpringBootApplication(scanBasePackages = {
+		"com.dewple.app_worker",
+		"com.dewple.common.config"
+})
 public class AppWorkerApplication {
 
 	public static void main(String[] args) {
